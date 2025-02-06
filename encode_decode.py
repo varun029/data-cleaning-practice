5702da248bb8b0745d66c3dd23c8fc986892c0d1
varun029 <varun.kaushik@jktech.com>
2025-02-06 14:42:03 +0530

    add encode decode exercise, use charset normalizer to find out the encoding type of file

---
+++ encode_decode.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+import charset_normalizer
+from utils import read
+
+np.random.seed(0)
+
+with open('./datasets/PoliceKillingsUS.csv','rb') as raw_data:
+    result=charset_normalizer.detect(raw_data.read(1000000))
+print(result)    
+
+police_killings=pd.read_csv('./datasets/PoliceKillingsUS.csv',encoding='windows-1250')
+
+print(police_killings.head())
